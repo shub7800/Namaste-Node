8c27e26b9a555a111663a339ac60650506348559
shubhamgupta780048@gmail.com <shubhamgupta780048@gmail.com>
2026-02-18 23:41:12 +0530

    learn require

---
+++ app.js
@@ -0,0 +1,11 @@
+const obj = require("./sum");
+
+require("./sum");
+const variable = "hello";
+console.log(variable);
+// var a =5;
+// var b= 6;
+
+// obj.calculate(a,b);
+
+console.log(obj.calculate(5,6));
---
+++ sum.js
@@ -0,0 +1,11 @@
+const x="hello"
+
+function calculate (a,b){
+    return a+b;
+
+}
+
+module.exports= {
+    x:x,
+    calculate:calculate,
+};
